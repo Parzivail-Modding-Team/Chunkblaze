d1daac4b42fb70f150434171dc1b8fc402b50f56
parzivail <colby.newman2000@gmail.com>
2019-09-24 13:03:52 -0400

    Initial commit.

---
+++ src/main/java/com/parzivail/chunkblaze/Chunkblaze.java
@@ -0,0 +1,32 @@
+package com.parzivail.chunkblaze;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Chunkblaze.MODID, name = Chunkblaze.NAME, version = Chunkblaze.VERSION)
+public class Chunkblaze
+{
+	public static final String MODID = "chunkblaze";
+	public static final String NAME = "Chunkblaze World Downloader";
+	public static final String VERSION = "1.0";
+
+	public static Logger logger;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		logger = event.getModLog();
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		MinecraftForge.EVENT_BUS.register(new ChunkblazeChunkInterceptor());
+
+		logger.info("Initialized.");
+	}
+}
