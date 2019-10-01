d9a5f8c77b4a20b0f1eb006a988105434ad8f152
parzivail <colby.newman2000@gmail.com>
2019-10-01 12:00:42 -0400

    save chunks under more conditions, save chunks when players enter them

--- src/main/java/com/parzivail/chunkblaze/handler/ChunkDaemon.java
+++ src/main/java/com/parzivail/chunkblaze/handler/ChunkDaemon.java
@@ -4,16 +4,21 @@ import com.google.common.collect.Maps;
 import com.parzivail.chunkblaze.Chunkblaze;
 import com.parzivail.chunkblaze.ChunkblazeConfig;
 import com.parzivail.chunkblaze.io.IOUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.IWorldEventListener;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.AnvilSaveHandler;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraft.world.storage.IThreadedFileIO;
 import net.minecraft.world.storage.ThreadedFileIOBase;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -28,6 +33,7 @@ public class ChunkDaemon implements IThreadedFileIO
 	private AnvilSaveHandler saveHandler;
 	private final Map<ChunkPos, NBTTagCompound> chunksToSave = Maps.newConcurrentMap();
 	private final Set<ChunkPos> chunksBeingSaved = Collections.newSetFromMap(Maps.newConcurrentMap());
+	private IWorldEventListener listener = new ChunkModifiedListener(this);
 
 	@SubscribeEvent
 	@SideOnly(Side.CLIENT)
@@ -42,40 +48,93 @@ public class ChunkDaemon implements IThreadedFileIO
 		{
 			workingWorld = world;
 			saveHandler = IOUtils.createSaveHandler(workingWorld);
+
+			world.removeEventListener(listener);
+			world.addEventListener(listener);
+
 			Chunkblaze.getLogger().info("Ready to save chunks in {}.", IOUtils.getWorldName());
 
 			Chunkblaze.Session.setRunning(ChunkblazeConfig.launchRunning);
 			Chunkblaze.Session.chunksMirrored = 0;
 		}
+	}
 
-		world.addEventListener(new ChunkModifiedListener(this));
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void on(TickEvent.WorldTickEvent args)
+	{
+		if (args.phase != TickEvent.Phase.END)
+			return;
+
+		Minecraft mc = Minecraft.getMinecraft();
+
+		EntityPlayer player = mc.player;
+
+		if (player == null)
+			return;
+
+		if (changedChunks(player))
+			saveChunk((int)player.posX >> 4, (int)player.posZ >> 4);
+	}
+
+	private boolean changedChunks(EntityPlayer player)
+	{
+		int chunkX = (int)player.posX >> 4;
+		int chunkZ = (int)player.posZ >> 4;
+		int prevChunkX = (int)player.prevPosX >> 4;
+		int prevChunkZ = (int)player.prevPosZ >> 4;
+
+		return chunkX != prevChunkX || chunkZ != prevChunkZ;
 	}
 
 	void saveChunk(int chunkX, int chunkZ)
 	{
+		if (!Chunkblaze.Session.isRunning())
+			return;
+
 		try
 		{
 			Chunk chunk = workingWorld.getChunkFromChunkCoords(chunkX, chunkZ);
 
-			NBTTagCompound nbttagcompound = new NBTTagCompound();
-			NBTTagCompound nbttagcompound1 = new NBTTagCompound();
-			nbttagcompound.setTag("Level", nbttagcompound1);
-			nbttagcompound.setInteger("DataVersion", 1343);
-			net.minecraftforge.fml.common.FMLCommonHandler.instance().getDataFixer().writeVersionData(nbttagcompound);
-			IOUtils.writeChunkToNBT(chunk, workingWorld, nbttagcompound1);
-			net.minecraftforge.common.ForgeChunkManager.storeChunkNBT(chunk, nbttagcompound1);
-			net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.event.world.ChunkDataEvent.Save(chunk, nbttagcompound));
-			enqueueChunkIO(chunk.getPos(), nbttagcompound);
+			ExtendedBlockStorage[] blocks = chunk.getBlockStorageArray();
+			int sections = countPopulatedSections(blocks);
+			if (sections == 0)
+			{
+				if (ChunkblazeConfig.verbose)
+					Chunkblaze.getLogger().info("Skipped empty chunk ({},{})/{}.", chunkX, chunkZ, workingWorld.provider.getDimensionType().getName());
+				return;
+			}
 
-			if (ChunkblazeConfig.verbose)
-				Chunkblaze.getLogger().info("Saved chunk ({},{})/{}.", chunkX, chunkZ, workingWorld.provider.getDimensionType().getName());
+			NBTTagCompound nbtChunkContainer = new NBTTagCompound();
+			NBTTagCompound nbtChunkData = new NBTTagCompound();
+			nbtChunkContainer.setTag("Level", nbtChunkData);
+			nbtChunkContainer.setInteger("DataVersion", 1343);
+			net.minecraftforge.fml.common.FMLCommonHandler.instance().getDataFixer().writeVersionData(nbtChunkContainer);
+			IOUtils.writeChunkToNBT(chunk, workingWorld, nbtChunkData);
+			net.minecraftforge.common.ForgeChunkManager.storeChunkNBT(chunk, nbtChunkData);
+			net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.event.world.ChunkDataEvent.Save(chunk, nbtChunkContainer));
+			enqueueChunkIO(chunk.getPos(), nbtChunkContainer);
 
 			Chunkblaze.Session.chunksMirrored++;
+
+			if (ChunkblazeConfig.verbose)
+				Chunkblaze.getLogger().info("Saved chunk ({},{})/{} ({} vertical chunks).", chunkX, chunkZ, workingWorld.provider.getDimensionType().getName(), sections);
 		}
 		catch (Exception exception)
 		{
 			Chunkblaze.getLogger().error(String.format("Failed to save chunk (%s,%s)/%s.", chunkX, chunkZ, workingWorld.provider.getDimensionType().getName()), exception);
 		}
+	}
+
+	private int countPopulatedSections(ExtendedBlockStorage[] blocks)
+	{
+		int sections = 0;
+
+		for (ExtendedBlockStorage b : blocks)
+			if (b != Chunk.NULL_BLOCK_STORAGE)
+				sections++;
+
+		return sections;
 	}
 
 	protected void enqueueChunkIO(ChunkPos pos, NBTTagCompound compound)
--- src/main/java/com/parzivail/chunkblaze/handler/ChunkModifiedListener.java
+++ src/main/java/com/parzivail/chunkblaze/handler/ChunkModifiedListener.java
@@ -1,6 +1,5 @@
 package com.parzivail.chunkblaze.handler;
 
-import com.parzivail.chunkblaze.Chunkblaze;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -30,15 +29,13 @@ public class ChunkModifiedListener implements IWorldEventListener
 	@Override
 	public void markBlockRangeForRenderUpdate(int x1, int y1, int z1, int x2, int y2, int z2)
 	{
-		if (!Chunkblaze.Session.isRunning())
-			return;
-
 		interceptor.saveChunk(x1 >> 4, z1 >> 4);
 	}
 
 	@Override
 	public void notifyBlockUpdate(World worldIn, BlockPos pos, IBlockState oldState, IBlockState newState, int flags)
 	{
+		interceptor.saveChunk(pos.getX() >> 4, pos.getZ() >> 4);
 	}
 
 	@Override
@@ -69,11 +66,13 @@ public class ChunkModifiedListener implements IWorldEventListener
 	@Override
 	public void onEntityAdded(Entity entityIn)
 	{
+		interceptor.saveChunk((int)entityIn.posX >> 4, (int)entityIn.posZ >> 4);
 	}
 
 	@Override
 	public void onEntityRemoved(Entity entityIn)
 	{
+		interceptor.saveChunk((int)entityIn.posX >> 4, (int)entityIn.posZ >> 4);
 	}
 
 	@Override
